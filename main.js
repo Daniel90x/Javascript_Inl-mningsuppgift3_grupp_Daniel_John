643491821ec61711869a55543b44e5c98f1eaa6b
John <john.a.nilsson@hotmail.com>
2021-02-19 17:06:01 +0100

    CriticColor fixad, Search on enter function Flexbox för film info Lite css/html pill
    Co-authored-by: Daniel Persson <daniel.persson18@learnet.se>

--- main.js
+++ main.js
@@ -7,21 +7,27 @@ const startButton = document.getElementById("searchButton");
 
 let response;
 
-startButton.addEventListener("click", async function(){
+document.getElementById("searchBar").addEventListener("keyup", function(event){
+    if (event.key === "Enter") {
+      callApi()
+    }
+  });
+
+startButton.addEventListener("click", callApi);
+
+async function callApi(){
     let search = document.getElementById("searchBar").value;
     response = await callMdApi(search);
     console.log(response);
 
     buildmainLayout();
-
-
-});
+}
 
 function buildmainLayout(){
 
 
     document.getElementById("filmTitel").innerHTML = response.Title;
-
+    criticColor();
     document.getElementById("criticId").innerHTML = response.Ratings[1].Value;
 
     document.getElementById("descriptionId").innerHTML = response.Plot;
@@ -32,5 +38,26 @@ function buildmainLayout(){
 
 
 
+}
+
+function criticColor(){
+    let str = response.Ratings[1].Value.slice(0,2);
+    var res = str.substring(str.length - 1, str.length);
+    if(res==="%"){
+        str = response.Ratings[1].Value.slice(0,1);
+    }
+    if(str <= 30){
+        document.getElementById("criticId").style.color = "red";
+
+    }
+
+    if(str > 30 && str < 70 ){
+        document.getElementById("criticId").style.color = "black";
+    }
+
+    if(str >= 70){
+        document.getElementById("criticId").style.color = "springgreen";
+
+    }
 
 }

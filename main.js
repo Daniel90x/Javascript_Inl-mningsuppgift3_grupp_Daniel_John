b414d4edbb48c1b7609175edebd177654c239004
Daniel Persson <daniel.persson18@learnet.se>
2021-02-19 12:03:29 +0100

    Mer html och js

--- main.js
+++ main.js
@@ -5,8 +5,33 @@
 
 const startButton = document.getElementById("searchButton");
 
-startButton.addEventListener("click", function(){
-    callMdApi();
+let response;
+
+startButton.addEventListener("click", async function(){
+    let search = document.getElementById("searchBar").value;
+    response = await callMdApi(search);
+    console.log(response);
+
+    buildmainLayout();
 
 
 });
+
+function buildmainLayout(){
+
+
+    document.getElementById("filmTitel").innerHTML = response.Title;
+
+    document.getElementById("criticId").innerHTML = response.Ratings[1].Value;
+
+    document.getElementById("descriptionId").innerHTML = response.Plot;
+
+
+
+    document.getElementById("poster").src=response.Poster;
+
+
+//flexTitle2
+
+
+}
--- omdbAPI.js
+++ omdbAPI.js
@@ -1,22 +1,70 @@
 'use strict'
 
 
+function getMovieDatabaseApi(search){
+    const omdbUrl = new URL ("http://www.omdbapi.com/?apikey=7fb63476")
+    omdbUrl.searchParams.append("page", "1");
+    omdbUrl.searchParams.append("r", "json");
+    omdbUrl.searchParams.append("s", search);
+    
 
-function getomdbApi(){
-    const omdbUrl = new URL ("http://www.omdbapi.com/?apikey=7fb63476&")// fel på api nycklen
+    return omdbUrl;
+}
+
+function getMovieDatabaseApiId(id){
+    const omdbUrl = new URL ("http://www.omdbapi.com/?apikey=7fb63476")
+    omdbUrl.searchParams.append("i", id);
+    omdbUrl.searchParams.append("r", "json");
+    omdbUrl.searchParams.append("plot", "full");
+    omdbUrl.searchParams.append("poster", "");
+
+    return omdbUrl;
+}
+
+async function callMdApi(search){
+    const omdbUrl = getMovieDatabaseApi(search);
+    const serverResponse = await fetch(omdbUrl, {
+	"method": "GET",
+})
+    const info = await serverResponse.json();
+    console.log(info);
+
+//Kallar på api med ID
+
+    const idUrl = getMovieDatabaseApiId(info.Search[0].imdbID);
+    const serverResponseId = await fetch(idUrl, {
+	"method": "GET",
+})
+    const infoId = await serverResponseId.json();
+    console.log(infoId);
+
+    return infoId;
+}
+
+
+
+
+
+
+/*function getomdbApi(){
+    const omdbUrl = new URL ("http://www.omdbapi.com/?apikey=7fb63476")// fel på api nycklen
+    omdbUrl.searchParams.append("i", "tt4154796");
     omdbUrl.searchParams.append("plot", "short");
     omdbUrl.searchParams.append("r", "json");
-    omdbUrl.searchParams.append("s", "inception");
-
     return omdbUrl;
 }
 
 async function callomdApi(){
     const url = getomdbApi();
-    const serverRespone = await fetch(url);
-    const info = await serverRespone.json;
+    const serverRespone = await fetch(url, {
+        "method": "GET"
+    })
+    const info = await serverRespone.json();
     console.log(info);
 }
 
-callomdApi();
+callomdApi();*/
+
+
+
 
